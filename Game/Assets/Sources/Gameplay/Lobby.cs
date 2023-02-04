710fd385574834ab4ce154d70e8409634063ea04
Fx73 <fx73000@yahoo.fr>
2023-02-04 19:14:06 +0100

    Menu Complété

---
+++ Game/Assets/Sources/Gameplay/Lobby.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Lobby:MonoBehaviour
+{
+    public static int MAX_PLAYER = 4;
+
+    static GameObject[] players = new GameObject[MAX_PLAYER];
+    static int playernumber = 0;
+
+
+    public void OnPlayerJoin( GameObject player)
+    {
+        Debug.Log("Player Joined");
+        player.transform.position = GetPosition(playernumber);
+        players[playernumber] = player;
+        playernumber++;
+
+        if (playernumber > 1)
+            GameObject.FindGameObjectWithTag("BtnGo").GetComponent<Button>().interactable = true;
+    }
+
+    public void OnPlayerLeft(GameObject player)
+    {
+        Debug.Log("Player Joined");
+        playernumber--;
+        Destroy(players[playernumber].gameObject);
+        players[playernumber] = null;
+
+
+        if (playernumber > 1)
+            GameObject.FindGameObjectWithTag("BtnGo").GetComponent<Button>().interactable = false;
+    }
+
+    private static Vector3 GetPosition(int n)
+    {
+        int posX = 6;
+        int posY = 3;
+
+        if (n == 0)
+            return new Vector3(-posX, posY, 0);
+        else if (n == 1)
+            return new Vector3(posX, posY, 0);
+        else if (n == 2)
+            return new Vector3(- posX, - posY, 0);
+        else if (n == 3)
+            return new Vector3(posX, - posY, 0);
+        else
+             return new Vector3(0, 0, 0);
+    }
+}
---
+++ Game/Assets/Sources/Standard/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{    
+
+        public void Change (string _nextScene)
+        {
+            SceneManager.LoadScene(_nextScene);
+        }
+
+ }
